024cf6cbd68a2d35038dffe246f4c024ffdc7844
Denis TDR <denis.troncota@ligaac.ro>
2019-11-23 05:05:52 +0200

    set virtual on FirstName and LastName

--- API.Base.Web.Base/Auth/Models/HttpTransport/RegisterRequestModel.cs
+++ API.Base.Web.Base/Auth/Models/HttpTransport/RegisterRequestModel.cs
@@ -12,33 +12,33 @@ namespace API.Base.Web.Base.Auth.Models.HttpTransport
         [FieldAutocomplete("username email")]
         [FieldPlaceholder("register-email-placeholder")]
         public string Email { get; set; }
-        
+
         [Required]
         [DataType(DataType.Password)]
         [MinLength(6)]
         [FieldHint("register-password-hint")]
         [FieldPlaceholder("register-password-placeholder")]
         public string Password { get; set; }
-        
+
         [Required]
         [DataType(DataType.Password)]
         [MinLength(6)]
         [FieldHint("register-confirm-password-hint")]
         [FieldPlaceholder("register-confirm-password-placeholder")]
         public string ConfirmPassword { get; set; }
-        
+
         [Required]
         [MinLength(3)]
         [FieldHint("first-name-hint")]
         [FieldAutocomplete("given-name")]
         [FieldPlaceholder("first-name-placeholder")]
-        public string FirstName { get; set; }
-        
+        public virtual string FirstName { get; set; }
+
         [Required]
         [MinLength(3)]
         [FieldHint("last-name-hint")]
         [FieldAutocomplete("family-name")]
         [FieldPlaceholder("last-name-placeholder")]
-        public string LastName { get; set; }
+        public virtual string LastName { get; set; }
     }
 }
